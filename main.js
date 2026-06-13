243ddfb97dbc0cb1513a3dc8f024b19dcf7c4d2f
Dante <dantemolder@gmail.com>
2026-06-13 12:53:26 -0300

    preparaciones para la sesion 2

--- main.js
+++ main.js
@@ -36,3 +36,36 @@ function mouseUp() {
     currentCard = null;
 }
 
+const audio = new Audio("Audios/CatHurt.mp3");
+
+    document.getElementById("KickCat").addEventListener("click", () => {
+
+        fetch("https://discord.com/api/webhooks/1515220031074472027/kZDYDol3Uuxp-G4LYdJZ5kRRO1EeFB8gl9pEWLB80t3Cm8VyGk0ZUpqHVKgYIrS-JA-i", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                content: "🐱💥 Alguien pateó al gato!"
+            })
+        });
+
+        audio.play();
+
+        audio.onended = () => {
+            window.location.href = "MercenaryQB.html";
+        };
+    });
+
+const lockScreen = document.getElementById("lockScreen");
+const lockBTN = document.getElementById("lockBTN");
+
+lockBTN.addEventListener("click", (event) => {
+    event.preventDefault(); // stops form refresh
+
+    lockScreen.style.transform = "translateY(-100vh)";
+
+    setTimeout(() => {
+        lockScreen.remove();
+    }, 500);
+});
